9971c2c075407590fe71745c53e1528e9b1d2a98
ysuzuki19 <msethuuh7@i.softbank.jp>
2019-03-19 10:10:23 +0900

    add argv to vector

---
+++ argvToVector/argvToVector.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+//int main(int argc, char **argv){
+int main(int argc, char *argv[]){
+  vector<string> str(argv, argv + argc);
+//  string str = to_string(argv[1]);
+  vector<int> x (0);
+  for(int i=0; i<str[1].length(); i++){
+    x.push_back(argv[1][i] - '0');
+  }
+
+  for(int i=0; i<x.size(); i++){
+    cout << x[i];
+  }
+  cout << endl;
+
+  return 0;
+}
