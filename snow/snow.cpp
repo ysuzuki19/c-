d5d6c1ddd754c7ab35c16514bd791bf2e5b5f1b9
ysuzuki19 <msethuuh7@i.softbank.jp>
2019-03-24 23:56:14 +0900

    add snow program

---
+++ snow/snow.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <random>
+using namespace std;
+#include <color.hpp>
+
+
+int snowwing(vector<vector<char> >&);
+int displaySnow(vector<vector<char> >&);
+
+int main(int argv, char **argc){
+  const int R = 100;
+  const int C = 50;
+  const int sleepTime = 10;
+  vector<vector<char> > snow (C, vector<char>(R,  ' '));
+
+  while(1){
+    snowwing(snow);
+    system("clear");
+    displaySnow(snow);
+    this_thread::sleep_for(chrono::milliseconds(sleepTime));
+  }
+  return 0;
+}
+
+
+int snowwing(vector<vector<char> > &snow){
+  for(int i=1; i<snow.size(); i++){
+      snow[snow.size()-i] = snow[snow.size()-i-1];
+  }
+  for(int i=0; i<snow.front().size(); i++){
+    if(rand()%20 == 0){
+      snow[0][i] = '*';
+    }else{
+      snow[0][i] = ' ';
+    }
+  }
+  return 0;
+}
+
+
+int displaySnow(vector<vector<char> > &snow){
+  for(int i=0; i<snow.size(); i++){
+    for(int j=0; j<snow.front().size(); j++){
+      cout << CYAN << snow[i][j];
+    }
+    cout << endl;
+  }
+  return 0;
+}
