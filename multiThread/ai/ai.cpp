e7f7352b81f3c92655a52df3e779f82d54e47fa9
ysuzuki19 <msethuuh7@i.softbank.jp>
2019-04-06 23:08:11 +0900

    add mutex lock mutliThread/ai/ai.cpp

--- multiThread/ai/ai.cpp
+++ multiThread/ai/ai.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <mutex>
 using namespace std;
 
 unsigned long long ai = 1;
 int flag = 1;
+mutex mtx;
 
 void man()
 {
   while(1){
-    if(flag == 1){
-      ai *= 2;
-      cout << "man   : 'I love you twice  as much as you. ' ---> Now ai is " << ai << endl;
-      std::this_thread::sleep_for(std::chrono::milliseconds(100));
-      flag = 0;
-      if(ai == 0){
-        break;
+    if(mtx.try_lock()){
+      if(flag == 1){
+        ai *= 2;
+        cout << "man   : 'I love you twice  as much as you. ' ---> Now ai is " << ai << endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        flag = 0;
+        if(ai == 0){
+          break;
+        }
       }
+      mtx.unlock();
     }
   }
 }
@@ -24,14 +29,17 @@ void man()
 void woman()
 {
   while(1){
-    if(flag == 0){
-      ai *= 3;
-      cout << "woman : 'I love you thrice as much as you. ' ---> Now ai is " << ai << endl;
-      std::this_thread::sleep_for(std::chrono::milliseconds(100));
-      flag = 1;
-      if(ai == 0){
-        break;
+    if(mtx.try_lock()){
+      if(flag == 0){
+        ai *= 3;
+        cout << "woman : 'I love you thrice as much as you. ' ---> Now ai is " << ai << endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        flag = 1;
+        if(ai == 0){
+          break;
+        }
       }
+      mtx.unlock();
     }
   }
 }
